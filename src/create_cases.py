ba728e236f32609f3ca94b3d848186433aaccef2
Gregor Olenik <go@hpsim.de>
2022-01-13 16:17:22 +0100

    Update create cases

--- src/create_cases.py
+++ src/create_cases.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 #
 
+
 def create_cases(arguments, config, logger):
-    # create case path
     # get build matrix
     # execute case recipe
+    case = arguments.get("case", None)
+    case_config = config.get("cases", {}).get(case, None)
+    if not case or not case_config:
+        return
 
-    for case in config.get("cases", []):
-        if submodule.get("build", False):
-            build_submodule(logger, submodule)
+    logger.info("creating case " + case)
+    if not os.path.exists(case):
+        os.mkdir(case)
+
+    build_matrix = case_config.get("values", [])
+
+    raw_build_command = case_config["build"]
+    steps = raw_build_command.split("\n")
+    if build_matrix:
+        for build_params in build_matrix:
+            for step in steps:
+                # TODO merge with build_submodule
+                values = re.findall(r"\${{values\.(\w+)}}", step)
+                for value in values:
+                    step = step.replace(
+                        "${{values." + value + "}}", build_params.get(value, "")
+                    )
