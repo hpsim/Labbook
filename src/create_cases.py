e69e51d802823f56dec87dd87f9ee4c52d092d61
Gregor Olenik <go@hpsim.de>
2022-01-13 19:56:49 +0100

    Iterate through cases

--- src/create_cases.py
+++ src/create_cases.py
@@ -11,32 +11,35 @@ def create_cases(arguments, config, logger):
     # execute case recipe
     file_logger = labbook_log.LogFile()
     case = arguments.get("case", None)
-    case_config = config.get("cases", {}).get(case, None)
-    if not case or not case_config:
+    cases_config = config.get("cases", [])
+    if not case or not cases_config:
         return
 
-    logger.info("creating case " + case)
-    if not os.path.exists(case):
-        os.mkdir(case)
+    for case_config in cases_config:
+        if not case_config["name"] == case:
+            continue
+        logger.info("creating case " + case)
+        if not os.path.exists(case):
+            os.mkdir(case)
 
-    build_matrix = case_config.get("values", [])
+        build_matrix = case_config.get("values", [])
 
-    raw_build_command = case_config["build"]
-    steps = raw_build_command.split("\n")
-    if build_matrix:
-        for build_params in build_matrix:
-            for step in steps:
-                # TODO merge with build_submodule
-                values = re.findall(r"\${{values\.(\w+)}}", step)
-                for value in values:
-                    step = step.replace(
-                        "${{values." + value + "}}", build_params.get(value, "")
-                    )
-            logger.info("Execute: " + step)
-            try:
-                ret = check_output(step.split(" "), cwd=path)
-                print(ret.decode("utf-8"))
+        raw_build_command = case_config["build"]
+        steps = raw_build_command.split("\n")
+        if build_matrix:
+            for build_params in build_matrix:
+                for step in steps:
+                    # TODO merge with build_submodule
+                    values = re.findall(r"\${{values\.(\w+)}}", step)
+                    for value in values:
+                        step = step.replace(
+                            "${{values." + value + "}}", build_params.get(value, "")
+                        )
+                logger.info("Execute: " + step)
+                try:
+                    ret = check_output(step.split(" "), cwd=path)
+                    print(ret.decode("utf-8"))
 
-            except subprocess.CalledProcessError as e:
-                print(e.output.decode("utf-8"))
-        file_logger.write(action="create_case", msg=case)
+                except subprocess.CalledProcessError as e:
+                    print(e.output.decode("utf-8"))
+            file_logger.write(action="create_case", msg=case)
