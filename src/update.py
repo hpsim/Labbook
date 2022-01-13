76600c190415436be1a3c6a30e1f7f8975d052ba
Gregor Olenik <go@hpsim.de>
2022-01-13 15:03:54 +0100

    Import logger

--- src/update.py
+++ src/update.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from subprocess import check_output
+import labbook_log
 
 
 def update(arguments, config, logger):
+    file_logger = labbook_log.LogFile()
+
     logger.info("Updating dependencies")
     check_output(["git", "submodule", "init"])
     check_output(["git", "submodule", "update"])
