32255168f520713d2afe1476dc97bf983719bcc7
Gregor Olenik <go@hpsim.de>
2022-01-17 09:18:21 +0100

    Add pipeline module

---
+++ src/pipeline.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from labbook_core import execute
+
+
+def execute(arguments, config, logger):
+    """init a new project labbook"""
+    pipeline = arguments["pipeline"]
+    case = arguments["case"]
+
+    raw_build_command = submodule["run"]
+    path = case
+    steps = raw_build_command.split("\n")
+
+    execute(steps, path, {}, logger)
