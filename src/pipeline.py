8d51a42c515fe6083edbf2b852e025d3641bb8cc
Gregor Olenik <gregor.olenik@web.de>
2022-06-24 21:08:50 +0200

    Add revision to path

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,14 +6,10 @@ import subprocess
 
 
 def get_campaign(config):
-    submodule = config["config"]["submodule"]
-    cmd = "git submodule foreach 'git branch --show-current'"
-    ret = subprocess(cmd.split(" ")).decode("utf-8").split("\n")
-    print(ret)
-    for i in range(len(ret) / 2, 2):
-        if submodule in ret[2 * i]:
-            return ret[2 * i + 1]
-    return False
+    submodule = config["config"]["campaign"]["submodule"]
+    with open(".git/modules/Dependencies/" + submodule + "/HEAD") as fh:
+        ret = fh.read()
+        return ret.split("/")[-1].replace("\n", "")
 
 
 def execute(arguments, config, logger):
@@ -26,6 +22,7 @@ def execute(arguments, config, logger):
         if not pipeline["name"] == pipeline_name:
             continue
 
+        logger.info("executing pipeline " + pipeline_name)
         raw_build_command = pipeline["run"]
         path = case
         steps = raw_build_command.split("\n")
@@ -36,11 +33,14 @@ def execute(arguments, config, logger):
         results = pipeline.get("results")
         if results:
             revision = list(labbook_core.get_revision(get_revision).keys())[0][0:8]
-            results_path = "results/" + campaign + "/" + case
+            results_path = "results/" + campaign + "/" + revision + "/" + case
             os.makedirs(results_path, exist_ok=True)
 
             for results_folder in results:
                 root, folder, files = next(os.walk(case + "/" + results_folder))
+                logger.info(
+                    "storing result folders " + str(folder) + " under " + results_path
+                )
                 for f in folder:
                     ret = subprocess.check_output(
                         ["cp", "-r", root + "/" + f, results_path]
